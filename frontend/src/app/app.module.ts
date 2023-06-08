337f7a54f8abd4815d2d467d241a7ce563f4be17
Donia Ahmed <pppopppo33@gmail.com>
2023-06-08 20:24:43 +0200

    First Commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodPageComponent
   ],
   imports: [
     BrowserModule,
