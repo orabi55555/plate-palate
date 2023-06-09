372af8ec34b16858086a2805a5dc8d884796a896
orabi55555 <elorabiahmed843@gmail.com>
2023-06-09 16:11:48 +0300

    food_thumbnails_withoutrating

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NgxStarRatingModule } from 'ngx-star-rating';
+import { RatingModule } from 'ngx-bootstrap/rating';
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbRatingModule,
+    NgxStarRatingModule,
+    RatingModule.forRoot(),
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
