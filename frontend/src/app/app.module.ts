d74e164f7b15c511d7101776afaebc21063547e4
weamibrahim <weamibrahim46@gmail.com>
2023-06-12 00:18:56 +0300

    update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
-import { NgxStarRatingModule } from 'ngx-star-rating';
-import { RatingModule } from 'ngx-bootstrap/rating';
+// import { NgxStarRatingModule } from 'ngx-star-rating';
+// import { RatingModule } from 'ngx-bootstrap/rating';
 
 @NgModule({
   declarations: [
@@ -19,9 +19,9 @@ import { RatingModule } from 'ngx-bootstrap/rating';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbRatingModule,
-    NgxStarRatingModule,
-    RatingModule.forRoot(),
+    // NgbRatingModule,
+    // NgxStarRatingModule,
+    // RatingModule.forRoot(),
 
 
 
