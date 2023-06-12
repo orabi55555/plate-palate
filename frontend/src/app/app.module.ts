ec6ce93b10ac4de590753b0e3fca186ec40f22b6
Donia Ahmed <pppopppo33@gmail.com>
2023-06-12 03:03:06 +0200

    First Commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpdateUserProfileComponent } from './components/update-user-profile/update-user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpdateUserProfileComponent
   ],
   imports: [
     BrowserModule,
