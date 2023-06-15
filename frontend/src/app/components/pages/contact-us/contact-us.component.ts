5a1069ade040095ff22d86682bd2e4d36816fd15
Donia Ahmed <pppopppo33@gmail.com>
2023-06-15 11:03:06 +0200

    update

--- frontend/src/app/components/pages/contact-us/contact-us.component.ts
+++ frontend/src/app/components/pages/contact-us/contact-us.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
-
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+  //Form Validables
+  registerForm: any = FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) {}
+  //Add user form actions
+  get f() {
+    return this.registerForm.controls;
+  }
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+    //True if all the fields are filled
+    if (this.submitted) {
+      alert('Great!!');
+    }
+  }
+  ngOnInit() {
+    //Add User form validations
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(60),
+        ],
+      ],
+    });
+  }
 
 }
