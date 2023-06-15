7fa06a9912add2b80615617a12d0cd485ad69c3b
weamibrahim <weamibrahim46@gmail.com>
2023-06-16 01:15:50 +0300

    make getbyid in food

--- backend/Controllers/FoodController.js
+++ backend/Controllers/FoodController.js
@@ -1,2 +1,14 @@
 const Food = require("../Models/FoodModel");
 
+exports.getFoodById = function(req, res) {
+  Food.findById(req.params.id)
+    .then(Food => {
+      if (!Food) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json( Food);
+    })
+    .catch(err => {
+      return res.status(500).json({ message: err.message });
+    });
+ };
