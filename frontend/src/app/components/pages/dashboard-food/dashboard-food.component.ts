6412f763c7ae501e85a7b0316b84056ae1ada1e0
weamibrahim <weamibrahim46@gmail.com>
2023-06-17 02:42:31 +0300

    get data from back

--- frontend/src/app/components/pages/dashboard-food/dashboard-food.component.ts
+++ frontend/src/app/components/pages/dashboard-food/dashboard-food.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard-food',
   templateUrl: './dashboard-food.component.html',
   styleUrls: ['./dashboard-food.component.css']
 })
-export class DashboardFoodComponent {
+export class DashboardFoodComponent implements OnInit {
+  foods: any[] | undefined;
+category: any;
+  constructor(private foodService: FoodService,private router: Router) { }
 
+  ngOnInit(): void {
+    this.foodService.getAllFoods().subscribe(data => {
+      this.foods = data;
+    });
+
+  }
 }
