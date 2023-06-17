c3a135bcfe7974da36cfad95bb22eeea8fbfc906
orabi55555 <elorabiahmed843@gmail.com>
2023-06-18 00:01:19 +0300

    signintoken solved

--- frontend/src/app/components/pages/auth/login-page/login-page.component.ts
+++ frontend/src/app/components/pages/auth/login-page/login-page.component.ts
@@ -1,36 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormControl,
   FormGroup,
   Validators,
   FormBuilder,
 } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css'],
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
   //Form Validables
   registerForm: any = FormGroup;
   submitted = false;
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {}
   //Add user form actions
   get f() {
     return this.registerForm.controls;
   }
-  onSubmit() {
-    this.submitted = true;
-    // stop here if form is invalid
-    if (this.registerForm.invalid) {
-      return;
-    }
-    //True if all the fields are filled
-    if (this.submitted) {
-      alert('Great!!');
-    }
-  }
+
   ngOnInit() {
     //Add User form validations
     this.registerForm = this.formBuilder.group({
@@ -44,5 +37,27 @@ export class LoginPageComponent {
         ],
       ],
     });
+
+
   }
-}
+
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+    //True if all the fields are filled
+    if (this.submitted) {
+      this.authService.login(this.registerForm.value).subscribe({
+        next: response => {console.log (response);
+                 this.router.navigateByUrl('/home');
+
+          // Login successful, do something with the response
+        },
+        error: error => {
+          // Login failed, handle the error
+        }
+      });}
+      // this.router.navigateByUrl('/home');
+}}
